141bcecd10c22893e39d1f40c0689ea887e7d74b
sean-leishman <leishmansean@gmail.com>
2023-11-09 18:08:34 +0000

    Additional boilerplate for future pathtracing

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
   std::string filename(
-      "/home/seanleishman/University/cg/cw2/materials/binary_primitves.json");
+      "/home/seanleishman/University/cg/cw2/materials/test1.json");
   Renderer renderer;
   renderer.load_file(filename);
   renderer.render_frame();
--- src/ray.cpp
+++ src/ray.cpp
@@ -1,5 +1,7 @@
 #include "ray.hpp"
 
+Ray::Ray(Vec3 origin ,Vec3 dir): origin(origin), direction(dir){};
+
 Vec3 Ray::point_at_parameter(float dist) const
 {
   return origin + direction * dist;
@@ -9,3 +11,8 @@ Vec3 Ray::point_at_parameter(Vec3 origin, Vec3 dir, float dist) const
 {
   return origin + dir * dist;
 }
+
+LightRay::LightRay(Vec3 origin, Vec3 dir): Ray(origin, dir){};
+
+ShadowRay::ShadowRay(Vec3 origin, Vec3 dir): Ray(origin, dir)
+{};
