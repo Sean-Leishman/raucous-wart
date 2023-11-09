89a003e1e46f9499cddfcdf1645c6992da6bab7c
sean-leishman <leishmansean@gmail.com>
2023-11-09 18:08:13 +0000

    Add classes for AmbientLight

--- include/light.hpp
+++ include/light.hpp
@@ -14,6 +14,7 @@ class Light{
       Vec3 intensity;
       PPMColor color;
 
+      Light(Vec3, Vec3, PPMColor);
       Light(Vec3, Vec3);
     virtual ~Light(){};
 
@@ -25,5 +26,13 @@ public:
     ~PointLight()=default;
     PointLight();
     PointLight(Vec3 position, Vec3 intensity);
+    float compute_intensity(float) override;
+};
+
+class AmbientLight: public Light {
+public:
+    ~AmbientLight()=default;
+    AmbientLight();
+    AmbientLight(Vec3 intensity, PPMColor color);
     float compute_intensity(float) override;
 };
--- src/light.cpp
+++ src/light.cpp
@@ -1,9 +1,13 @@
 #include "light.hpp"
 #include "vector.hpp"
 
+Light::Light(Vec3 position, Vec3 intensity, PPMColor color): position(position), intensity(intensity), color(color){};
 Light::Light(Vec3 position, Vec3 intensity): position(position), intensity(intensity), color(PPMColor(254, 255, 255)){}
 
 PointLight::PointLight(Vec3 position, Vec3 intensity): Light(position, intensity){};
 float PointLight::compute_intensity( float NdotL) {
  return std::max(std::min(NdotL,1.0f), 0.0f);
 }
+
+AmbientLight::AmbientLight(Vec3 intensity, PPMColor color): Light(Vec3(), intensity, color){}
+float AmbientLight::compute_intensity(float) { return 0; };
